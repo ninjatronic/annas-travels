eeb252a4677e223006e858bb6e3b85bc0c025705
Pete Martin <pete@ninjatronic.com>
2013-12-04 21:03:37 +0000

    working on the loop
    post excerpts, comment counts and permalinks in the loop in index.php

--- functions.php
+++ functions.php
@@ -2,4 +2,27 @@
 
 add_theme_support( 'post-thumbnails' );
 
+function wp_new_excerpt($text)
+{
+    if ($text == '')
+    {
+        $text = get_the_content('');
+        $text = strip_shortcodes( $text );
+        $text = apply_filters('the_content', $text);
+        $text = str_replace(']]>', ']]>', $text);
+        $text = strip_tags($text);
+        $text = nl2br($text);
+        $excerpt_length = apply_filters('excerpt_length', 55);
+        $words = explode(' ', $text, $excerpt_length + 1);
+        if (count($words) > $excerpt_length) {
+            array_pop($words);
+            array_push($words, '');
+            $text = implode(' ', $words);
+        }
+    }
+    return $text;
+}
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'wp_new_excerpt');
+
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,54 @@
 <?php get_header(); ?>
 
 <div class="container">
+
     <div class="row">
-        <h1>
-            Content
-        </h1>
+        <div class="col-sm-9">
+
+            <div class="row">
+                <div class="panel">
+                    <h1 class="tk-estilo-pro big-light">
+                        Recent Posts
+                    </h1>
+                </div>
+            </div>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div class="row">
+                    <div class="panel">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <!-- post image -->
+                            </div>
+                            <div class="col-sm-9">
+                                <h2 class="tk-estilo-pro">
+                                    <?php the_title() ?>
+                                </h2>
+                                <p class="tk-museo-sans justify lead">
+                                    <?php echo get_the_excerpt(); ?>
+                                </p>
+                                <p class="tk-museo-sans">
+                                    <?php echo get_comments_number() ?> Comments | <a href="<?php the_permalink(); ?>">Read More...</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; else: ?>
+                <div class="row">
+                    <div class="panel">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+        <div class="col-sm-3">
+            <!-- sidebar -->
+            <?php get_sidebar(); ?>
+        </div>
     </div>
 </div>
 
